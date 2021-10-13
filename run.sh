b691f3a49f18c99a5042e73042f8133e1f9f2f15
Brandon Cchung <bctcchung@gmail.com>
2021-10-13 21:36:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,8 @@ WORKDIR=.
 OUTPUTFILE=./output.log
 
 # Processing Variables
-STYLEAMOUNT=3 # max amount of style images per style transfer
+STYLEMINAMOUNT=1 # min amount of style images per style transfer
+STYLEMAXAMOUNT=3 # max amount of style images per style transfer
 IMAGESIZE=480 # max image size in px
 
 ## Install the packages
@@ -21,7 +22,7 @@ apt update -y && apt install zip -y
 for file in `ls $CONTENTDIR`
 do
     # Get n random style files
-    RANDOMNUMBER=$(shuf -i1-$STYLEAMOUNT -n1)
+    RANDOMNUMBER=$(shuf -i$STYLEMINAMOUNT-$STYLEMAXAMOUNT -n1)
     RANDOMSTYLEFILE=$(find $STYLEDIR -type f | shuf -n $RANDOMNUMBER | tr '\n' ' ')
     JSONINPUT=$(echo "$RANDOMSTYLEFILE" | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
     # Run the style transfer with a randomized amount of styles
