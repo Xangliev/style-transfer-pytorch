c1f8c4063a16451c64dc4f69f97c0c831685c9ce
Brandon Cchung <bctcchung@gmail.com>
2021-10-18 19:30:07 +0200

    Update run-repeat.sh

--- run-repeat.sh
+++ run-repeat.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# applies style-transfer to existing content image in a loop using style images
+# applies style-transfer to existing content images using style images
 
 read -p 'Where to store results? ' RESULTDIR
-read -p 'Repeat process how many times?' LOOPS
 
 # Directory & File variables
 CONTENTDIR=content # directory where content is stored
 STYLEDIR=styles # directory where styles are stored
-WORKDIR=.
-CONTENTEXTENSION=jpg
 
 OUTPUTFILE=output.log
 
@@ -18,35 +15,54 @@ STYLEMAXAMOUNT=3 # max amount of style images per style transfer
 IMAGESIZE=512 # max image size in px
 
 ## Install the packages
-pip install -e $WORKDIR
+pip install -e .
 apt update -y && apt install zip -y
 
 ## Run the model for each contentfile
-for i in $(seq 1 $LOOPS)
+for file in `ls $CONTENTDIR`
 do
     # Remove notebook checkpoint folder
-    rm -rf $STYLEDIR/.ipynb_checkpoints
+    find $STYLEDIR -type f -name '.ipynb_checkpoints' -delete
+
     # Get n random style files
     RANDOMNUMBER=$(shuf -i$STYLEMINAMOUNT-$STYLEMAXAMOUNT -n1)
-    RANDOMSTYLEFILE=$(find $STYLEDIR -type f | shuf -n $RANDOMNUMBER | tr '\n' ' ')
-    JSONINPUT=$(echo "$RANDOMSTYLEFILE" | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
-    # Run the style transfer with a randomized amount of styles
-    style_transfer $CONTENTDIR/${i}.$jpg $RANDOMSTYLEFILE -o $RESULTDIR/${i}.png -s $IMAGESIZE
-    if [[ ${#i} -eq 1 ]]
-        then
-            NAME="000$i"
-    elif [[ ${#i} -eq 2 ]]
-        then
-            NAME=="00$i"
-    elif [[ ${#i} -eq 3 ]]
-        then
-            NAME=="0$i"
-    else
-        NAME=="$i"
-    fi
+
+    STYLELIST=""
+    STYLEARRAY=()
+
+    # Loop through directory n times
+    for i in $(seq 1 $RANDOMNUMBER)
+    do
+        # Select random rarity folder
+        RANDOMFOLDER=$(shuf -i1-100 -n1)
+        if [ $RANDOMFOLDER -lt 55 ]; then
+            RARITYFOLDER=Common
+        elif [ $RANDOMFOLDER -lt 85 ] ; then
+            RARITYFOLDER=Uncommon
+        elif [ $RANDOMFOLDER -lt 95 ] ; then
+            RARITYFOLDER=Rare
+        elif [ $RANDOMFOLDER -lt 100 ] ; then
+            RARITYFOLDER=Superrare
+        else
+            RARITYFOLDER=Ultrarare
+        fi
+
+        RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+        while [[ "${STYLEARRAY[*]}" =~ "${RANDOMSTYLEFILE}" ]];do
+          RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+        done
+        STYLELIST=${STYLELIST}${RANDOMSTYLEFILE}
+        STYLEARRAY[${#STYLEARRAY[@]}]=$RANDOMSTYLEFILE
+    done
+
+    JSONINPUT=$(echo "$STYLELIST" | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
     
 # Output result to logfile
 cat <<EOF >> $OUTPUTFILE
-{"name": "$NAME", "styles": "[$JSONINPUT]", "style_amount": "$RANDOMNUMBER"},
+{"name": "$file", "styles": "[$JSONINPUT]", "style_amount": "$RANDOMNUMBER"},
 EOF
+
+    # Run the style transfer with a randomized amount of styles
+    style_transfer $CONTENTDIR/$file $STYLELIST -o $RESULTDIR/$file -s $IMAGESIZE
+
 done
