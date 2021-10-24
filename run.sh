eda73fa2944a90d371b220b1e377697ccdb1c319
Brandon Cchung <bctcchung@gmail.com>
2021-10-24 21:26:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,8 @@ OUTPUTFILE=output.log
 
 # Processing Variables
 IMAGESIZE=480 # max image size in px
+STYLEMINAMOUNT=1 # min amount of complex style images per style transfer
+STYLEMAXAMOUNT=2 # max amount of complex style images per style transfer
 
 ## Install the packages
 pip install -e .
@@ -27,49 +29,53 @@ do
         # Remove notebook checkpoint folders
         find $STYLEDIR -type d -name '.ipynb_checkpoints' -exec rm -rf {} +
 
+        # Get n random style files
+        RANDOMNUMBER=$(shuf -i$STYLEMINAMOUNT-$STYLEMAXAMOUNT -n1)
+
         STYLELIST=""
+        STYLEARRAY=()
+        
+        # Loop through directory n times
+        for i in $(seq 1 $RANDOMNUMBER)
+        do
+            # Select random complex rarity folder
+            RANDOMFOLDER=$(shuf -i1-100 -n1)
+            if [ $RANDOMFOLDER -lt 50 ]; then
+                COMPLEXFOLDER=common
+            elif [ $RANDOMFOLDER -lt 85 ] ; then
+                COMPLEXFOLDER=uncommon
+            elif [ $RANDOMFOLDER -lt 95 ] ; then
+                COMPLEXFOLDER=rare
+            elif [ $RANDOMFOLDER -lt 100 ] ; then
+                COMPLEXFOLDER=superrare
+            else
+                COMPLEXFOLDER=ultrarare
+            fi
 
-        # Select random simple rarity folder
-        RANDOMSIMPLE=$(shuf -i1-100 -n1)
-        if [ $RANDOMSIMPLE -lt 50 ]; then
-            SIMPLEFOLDER=common
-        elif [ $RANDOMSIMPLE -lt 85 ] ; then
-            SIMPLEFOLDER=uncommon
-        elif [ $RANDOMSIMPLE -lt 95 ] ; then
-            SIMPLEFOLDER=rare
-        elif [ $RANDOMSIMPLE -lt 100 ] ; then
-            SIMPLEFOLDER=superrare
-        else
-            SIMPLEFOLDER=ultrarare
-        fi
+            # Append unique complex style(s)
+            RANDOMCOMPLEXSTYLE=$(find $STYLEDIR/complex/$COMPLEXFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+            while [[ "${STYLEARRAY[*]}" =~ "${RANDOMCOMPLEXSTYLE}" ]];do
+              RANDOMCOMPLEXSTYLE=$(find $STYLEDIR/complex/$COMPLEXFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+            done
+            STYLELIST=${STYLELIST}${RANDOMCOMPLEXSTYLE}
+            STYLEARRAY[${#STYLEARRAY[@]}]=$RANDOMCOMPLEXSTYLE
+        done
 
-        # Select random complex rarity folder
-        RANDOMCOMPLEX=$(shuf -i1-100 -n1)
-        if [ $RANDOMCOMPLEX -lt 50 ]; then
-            COMPLEXFOLDER=common
-        elif [ $RANDOMCOMPLEX -lt 85 ] ; then
-            COMPLEXFOLDER=uncommon
-        elif [ $RANDOMCOMPLEX -lt 95 ] ; then
-            COMPLEXFOLDER=rare
-        elif [ $RANDOMCOMPLEX -lt 100 ] ; then
-            COMPLEXFOLDER=superrare
-        else
-            COMPLEXFOLDER=ultrarare
-        fi
-
-        RANDOMSIMPLESTYLE=$(find $STYLEDIR/simple/$SIMPLEFOLDER -type f | shuf -n 1 | tr '\n' ' ')
-        RANDOMCOMPLEXSTYLE=$(find $STYLEDIR/complex/$COMPLEXFOLDER -type f | shuf -n 1 | tr '\n' ' ')
-        STYLELIST=${RANDOMSIMPLESTYLE}${RANDOMCOMPLEXSTYLE}
-
+        # Append random simple style
+        RANDOMSIMPLESTYLE=$(find $STYLEDIR/simple -type f | shuf -n 1 | tr '\n' ' ')
+        STYLELIST=${STYLELIST}${RANDOMSIMPLESTYLE}
         JSONINPUT=$(echo $STYLELIST | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
+        
+        # Create random ID
+        RANDOMID=$(shuf -i1-10000 -n1)
     
 # Output result to logfile
 cat <<EOF >> $OUTPUTFILE
-{"name": "${RANDOMSIMPLE}-${RANDOMCOMPLEX}-$file", "styles": "[$JSONINPUT]"},
+{"name": "${RANDOMID}-$file", "styles": "[$JSONINPUT]"},
 EOF
 
         # Run the style transfer with a randomized amount of styles
-        style_transfer $CONTENTDIR/$file $STYLELIST -o $RESULTDIR/${RANDOMSIMPLE}-${RANDOMCOMPLEX}-${file} -s $IMAGESIZE
+        style_transfer $CONTENTDIR/$file $STYLELIST -o $RESULTDIR/${RANDOMID}-${file} -s $IMAGESIZE
         
         # Optionally remove content file
         if [ "$REMOVE" == 'yes' ]
