c06861697bc7630bda80e6dc6c84de0dfa45749a
Brandon Cchung <bctcchung@gmail.com>
2021-10-28 16:52:30 +0200

    Update rarities.sh

--- rarities.sh
+++ rarities.sh
@@ -2,6 +2,7 @@
 # applies style-transfer to existing content images using style images
 
 read -p 'Where to store results? ' RESULTDIR
+read -p 'How many loops? (default: 1) ' LOOPS
 read -p 'Remove content files after processing? (yes/no default: no) ' REMOVE
 
 # Directory & File variables
@@ -18,58 +19,63 @@ IMAGESIZE=512 # max image size in px
 ## Install the packages
 pip install -e .
 apt update -y && apt install zip -y
+pip install --upgrade --force-reinstall pillow
 
-## Run the model for each contentfile
-for file in `ls $CONTENTDIR`
+#loop X times
+for iteration in $(seq 1 $LOOPS)
 do
-    # Remove notebook checkpoint folders
-    find $STYLEDIR -type d -name '.ipynb_checkpoints' -exec rm -rf {} +
-
-    # Get n random style files
-    RANDOMNUMBER=$(shuf -i$STYLEMINAMOUNT-$STYLEMAXAMOUNT -n1)
-
-    STYLELIST=""
-    STYLEARRAY=()
-
-    # Loop through directory n times
-    for i in $(seq 1 $RANDOMNUMBER)
+    ## Run the model for each contentfile
+    for file in `ls $CONTENTDIR`
     do
-        # Select random rarity folder
-        RANDOMFOLDER=$(shuf -i1-100 -n1)
-        if [ $RANDOMFOLDER -lt 50 ]; then
-            RARITYFOLDER=common
-        elif [ $RANDOMFOLDER -lt 85 ] ; then
-            RARITYFOLDER=uncommon
-        elif [ $RANDOMFOLDER -lt 95 ] ; then
-            RARITYFOLDER=rare
-        elif [ $RANDOMFOLDER -lt 100 ] ; then
-            RARITYFOLDER=superrare
-        else
-            RARITYFOLDER=ultrarare
-        fi
+        # Remove notebook checkpoint folders
+        find $STYLEDIR -type d -name '.ipynb_checkpoints' -exec rm -rf {} +
 
-        RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
-        while [[ "${STYLEARRAY[*]}" =~ "${RANDOMSTYLEFILE}" ]];do
-          RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+        # Get n random style files
+        RANDOMNUMBER=$(shuf -i$STYLEMINAMOUNT-$STYLEMAXAMOUNT -n1)
+
+        STYLELIST=""
+        STYLEARRAY=()
+
+        # Loop through directory n times
+        for i in $(seq 1 $RANDOMNUMBER)
+        do
+            # Select random rarity folder
+            RANDOMFOLDER=$(shuf -i1-100 -n1)
+            if [ $RANDOMFOLDER -lt 50 ]; then
+                RARITYFOLDER=common
+            elif [ $RANDOMFOLDER -lt 85 ] ; then
+                RARITYFOLDER=uncommon
+            elif [ $RANDOMFOLDER -lt 95 ] ; then
+                RARITYFOLDER=rare
+            elif [ $RANDOMFOLDER -lt 100 ] ; then
+                RARITYFOLDER=superrare
+            else
+                RARITYFOLDER=ultrarare
+            fi
+
+            RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+            while [[ "${STYLEARRAY[*]}" =~ "${RANDOMSTYLEFILE}" ]];do
+            RANDOMSTYLEFILE=$(find $STYLEDIR/$RARITYFOLDER -type f | shuf -n 1 | tr '\n' ' ')
+            done
+            STYLELIST=${STYLELIST}${RANDOMSTYLEFILE}
+            STYLEARRAY[${#STYLEARRAY[@]}]=$RANDOMSTYLEFILE
         done
-        STYLELIST=${STYLELIST}${RANDOMSTYLEFILE}
-        STYLEARRAY[${#STYLEARRAY[@]}]=$RANDOMSTYLEFILE
-    done
 
-    JSONINPUT=$(echo "$STYLELIST" | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
-    
+        JSONINPUT=$(echo "$STYLELIST" | awk '{for(i=1;i<NF;i++)if(i!=NF){$i=$i","}  }1' | awk '{$1=$1};1')
+        
 # Output result to logfile
 cat <<EOF >> $OUTPUTFILE
 {"name": "$file", "styles": "[$JSONINPUT]", "style_amount": "$RANDOMNUMBER"},
 EOF
 
-    # Run the style transfer with a randomized amount of styles
-    style_transfer $CONTENTDIR/$file $STYLELIST -o $RESULTDIR/$file -s $IMAGESIZE
-    
-    # Optionally remove content file
-    if [ "$REMOVE" == 'yes' ]
-    then
-        rm $CONTENTDIR/$file
-    fi
+        # Run the style transfer with a randomized amount of styles
+        style_transfer $CONTENTDIR/$file $STYLELIST -o $RESULTDIR/$file -s $IMAGESIZE
+        
+        # Optionally remove content file
+        if [ "$REMOVE" == 'yes' ]
+        then
+            rm $CONTENTDIR/$file
+        fi
 
+    done
 done
